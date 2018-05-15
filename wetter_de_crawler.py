4fc04f4f9f66dc5d5af3fe144a572e1a5c29703a
Daniel Wujecki <daniel.wujecki@campus.tu-berlin.de>
2018-05-15 15:06:07 +0200

    suche nach plz moeglich

--- wetter_de_crawler.py
+++ wetter_de_crawler.py
@@ -8,20 +8,43 @@ from socket import timeout
 from lxml import etree
 
 
-def get_bundeslaender():
-    url = 'https://www.wetter.de/deutschland'
+def city_url(plz: int) -> str:
+    """
+    Sucht auf wetter.de nach Stadt mit Postleitzahl und gibt
+    Link mit URL zur aktuellen Tagesübersicht zurück
+
+    :param plz: 5 stellige Postleitzahl deutscher Städte als Int
+    :return: url mit Tagesübersicht für Stadt mit Postleitzahl
+    """
+
+    plz = str(plz)
+    if len(plz) != 5:
+        raise ValueError("Postcode isn't valid!")
+
+    url = 'https://www.wetter.de/suche.html?search=' + plz.strip()
     code = urlopen(url, timeout=5)
 
     parser = etree.HTMLParser()
     tree = etree.parse(code, parser)
+    links = tree.xpath("//a[starts-with(@id,'location-')]/@href")
 
-    links = tree.xpath("//a[@id='menu-map-country-deutschland']")[0]
-    links = links.xpath("//a[starts-with(@id,'menu-province-')]/@href")
+    url = links[0]
+    url = url.replace(".html", "/wetterbericht-aktuell.html")
 
-    return links
+    return url
+
+
+def parse_city(url: str):
+    code = urlopen(url, timeout=5)
+    parser = etree.HTMLParser()
+    tree = etree.parse(code, parser)
 
 
 def main():
+    url = city_url(14656)
+    print(url)
+    parse_city(url)
+
     try:
         pass
     except URLError:
@@ -33,5 +56,5 @@ def main():
 
 
 if __name__ == "__main__":
-    # main()
-    print("Not working at the moment.")
+    main()
+    # print("Not working at the moment.")
