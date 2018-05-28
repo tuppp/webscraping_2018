23cbc74033ceae441797931e6d73923feed80667
Alessandro Schneider <alessandro.schneider.privat@gmail.com>
2018-05-28 11:11:12 +0200

    runClient bash

--- runClient.py
+++ runClient.py
@@ -1,12 +1,16 @@
+'''
+is script is started with runClient.bash due to anaconda env needed for certain imports
 #!/home/webcrawling/miniconda3/bin/python
 # -*- coding: utf-8 -*-
+'''
 
-# from OpenWheatherMapRestClient import *
 import openweathermap_org_API_client
 
 
 def run():
+    ''' run scripts like this. handle plz internally. sorry daniel for changes here.. '''
     openweathermap_org_API_client.run()
+
 
 
 if __name__ == "__main__":
