33238270bf90e60f6cc1b86c18dc1161a662744d
Valentin Lietz <valentinlietz@wlan-141-23-189-108.tubit.tu-berlin.de>
2018-05-29 14:15:29 +0200

    Added wetterdienst_de.start() to runClient.py

--- runClient.py
+++ runClient.py
@@ -5,12 +5,12 @@ is script is started with runClient.bash due to anaconda env needed for certain 
 '''
 
 import openweathermap_org_API_client
-
+import wetterdienst_de
 
 def run():
     ''' run scripts like this. handle plz internally. sorry daniel for changes here.. '''
     openweathermap_org_API_client.run()
-
+    wetterdienst_de.start()
 
 
 if __name__ == "__main__":
