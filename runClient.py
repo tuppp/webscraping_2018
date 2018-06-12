6887c5f6cc575ef1051c4f844e01d1faba34571a
codelessandro <alessandro.schneider.signup@gmail.com>
2018-06-12 14:10:48 +0200

    aenderungen auf server zum git hinzufuegen..... .gitignore angepasst

--- runClient.py
+++ runClient.py
@@ -8,13 +8,21 @@ import openweathermap_org_API_client
 import wetterdienst_de
 import accuweather_com_API_client
 import wetter_com_crawler
+import datetime
 
 def run():
     print("hola")
     ''' run scripts like this. handle plz internally. sorry daniel for changes here.. '''
+    print("Acu:" + str(datetime.datetime.now()))
     accuweather_com_API_client.run()
+
+    print("Wetterdienst:" + str(datetime.datetime.now()))
     wetterdienst_de.start()
+
+    print("Openweathermap" + str(datetime.datetime.now()))
     openweathermap_org_API_client.run()
+
+    print("WEtter.com" + str(datetime.datetime.now()))
     wetter_com_crawler.start()
 
 
