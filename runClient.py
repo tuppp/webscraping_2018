e46749d26adcca67dd3e55d3c858de3d79370d8d
Daniel Wujecki <daniel.wujecki@campus.tu-berlin.de>
2018-07-09 13:05:51 +0200

    kleine anpassungen in runClient

--- runClient.py
+++ runClient.py
@@ -10,22 +10,19 @@ import accuweather_com_API_client
 import wetter_com_crawler
 import datetime
 
-def run():
 
+def run():
     print("Wetterdienst:" + str(datetime.datetime.now()))
     wetterdienst_de.start()
 
     print("WEtter.com" + str(datetime.datetime.now()))
     wetter_com_crawler.start()
 
-
     print("Openweathermap" + str(datetime.datetime.now()))
     openweathermap_org_API_client.run()
 
     print("Acu:" + str(datetime.datetime.now()))
     accuweather_com_API_client.run()
-
-
 
 
 if __name__ == "__main__":
