61f013533f47849d69e0fbb6bc9e171f4e35bc64
Daniel Wujecki <daniel.wujecki@campus.tu-berlin.de>
2018-07-10 11:55:59 +0200

    An PEP 8 Standard angepasst

--- openweathermap_org_API_client.py
+++ openweathermap_org_API_client.py
@@ -9,56 +9,67 @@ alexander franz 358870
 daniel hartung 338991    
 """
 
-import requests
-import datetime
 import os
-import json
 import time
+import datetime
+
+import requests
+import json
+# import pdb
+
 import saveCSVModel as saveModel
-#import pdb
 
-#names and paths for saving data
-dateString = datetime.datetime.today().strftime('%Y-%m-%d');
-localJsonSavePath = "openweather"+dateString+".txt";
-websiteName = "http://api.openweathermap.org";
-websiteNameShort = "openweathermaporg";
+# names and paths for saving data
+dateString = datetime.datetime.today().strftime('%Y-%m-%d')
+localJsonSavePath = "openweather"+dateString+".txt"
+websiteName = "http://api.openweathermap.org"
+websiteNameShort = "openweathermaporg"
 
 
-#functions for  converting units to metric system
+# functions for  converting units to metric system
 def kelvin_to_celcius(k_temp):
-        return (k_temp - 273.15)
-def meter_per_second_to_km_per_h(speed):
-    return 3.6 * speed;
+        return k_temp - 273.15
 
-#saves raw json response for backup purposes -> maybe use other parts later on?
+
+def meter_per_second_to_km_per_h(speed):
+    return 3.6 * speed
+
+
 def save_raw_json_response(obj):
+    """
+    saves raw json response for backup purposes -> maybe use other parts later on?
+    """
+
     if not os.path.exists("raw_data"):
         os.makedirs("raw_data")
 
-    if (os.path.exists("raw_data/" + localJsonSavePath)):
+    if os.path.exists("raw_data/" + localJsonSavePath):
         with open("raw_data/" + localJsonSavePath, 'a') as outfile:
-            json.dump(obj, outfile);
+            json.dump(obj, outfile)
     else:
-        with open("raw_data/" +localJsonSavePath, 'w') as outfile:
-            json.dump(obj, outfile);
+        with open("raw_data/" + localJsonSavePath, 'w') as outfile:
+            json.dump(obj, outfile)
 
-#extracts all the data out of json and saves it 
+
 def extract_and_save_data(obj, file, zip_code):
-    stadt = obj["city"]["name"];
-    obj = obj["list"];
+    """
+    extracts all the data out of json and saves it
+    """
+
+    stadt = obj["city"]["name"]
+    obj = obj["list"]
     
     for entry in obj:
-        #extract data from response
-        timestamp = entry["dt"];
-        temperatur = kelvin_to_celcius(entry["main"]["temp"]);
-        mintemperatur = kelvin_to_celcius(entry["main"]["temp_min"]);
-        maxtemperatur = kelvin_to_celcius(entry["main"]["temp_max"]);
-        windgeschwindigkeit = entry["wind"]["speed"];
-        windgeschwindigkeit = meter_per_second_to_km_per_h(windgeschwindigkeit);
-        luftdruck = entry["main"]["grnd_level"];
-        
-        
-        #save relevant data to csv file
+        # extract data from response
+        timestamp = entry["dt"]
+        temperatur = kelvin_to_celcius(entry["main"]["temp"])
+        mintemperatur = kelvin_to_celcius(entry["main"]["temp_min"])
+        maxtemperatur = kelvin_to_celcius(entry["main"]["temp_max"])
+        windgeschwindigkeit = entry["wind"]["speed"]
+        windgeschwindigkeit = meter_per_second_to_km_per_h(windgeschwindigkeit)
+        luftdruck = entry["main"]["grnd_level"]
+
+        # save relevant data to csv file
         file.save(websiteNameShort, websiteName,
                   time.time(), float(timestamp),
                   zip_code, stadt,
@@ -69,29 +80,31 @@ def extract_and_save_data(obj, file, zip_code):
                   None, None)
 
 
-#main
-def getOpenWeatherMapData():
-
+def run():
+    """
+    main
+    """
     file_object = open("ZIP_Codes", "r")
     zip_codes = file_object.readlines()
     url = "http://api.openweathermap.org/data/2.5/forecast?APPID=428ef9d699cb5963b396bd10215f2d3f&zip="
     url_appendix = ",de"
     file = saveModel.saveData()
 
-    #do API call for all locations
-    for index,zip_code in enumerate(zip_codes):
+    # do API call for all locations
+    for index, zip_code in enumerate(zip_codes):
         if index % 60 == 0 and index != 0:
-           time.sleep(61)
+            time.sleep(61)
 
-        myResponse = requests.get(url + zip_code.rstrip() + url_appendix)
+        my_response = requests.get(url + zip_code.rstrip() + url_appendix)
 
-        #only write into file, when zip code is available
-        if(myResponse.ok):
-            obj = myResponse.json()
+        # only write into file, when zip code is available
+        if my_response.ok:
+            obj = my_response.json()
             save_raw_json_response(obj)
-            extract_and_save_data(obj,file,zip_code.rstrip())
+            extract_and_save_data(obj, file, zip_code.rstrip())
 
     file.close()
 
-def run():
-    getOpenWeatherMapData()
+
+if __name__ == "__main__":
+    run()
