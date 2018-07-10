286e3edd88dcb03a57319b299335e2e36b1a2324
Daniel Wujecki <daniel.wujecki@campus.tu-berlin.de>
2018-07-10 14:31:34 +0200

    anaconda direkt verwenden - kein shell skript mehr notwendig

--- runClient.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PATH="/home/webcrawling/miniconda3/bin:$PATH"
-source activate mkp
-cd /home/webcrawling/webscraping_2018/
-python  runClient.py
--- runClient.py
+++ runClient.py
@@ -1,21 +1,24 @@
-'''
-is script is started with runClient.bash due to anaconda env needed for certain imports
-#!/home/webcrawling/miniconda3/bin/python
+#!/home/webcrawling/miniconda3/envs/mkp/bin/python
 # -*- coding: utf-8 -*-
-'''
+
+"""
+anaconda env needed for certain imports
+"""
+
+import os
+import datetime
 
 import openweathermap_org_API_client
 import wetterdienst_de
 import accuweather_com_API_client
 import wetter_com_crawler
-import datetime
 
 
 def run():
     print("Wetterdienst:" + str(datetime.datetime.now()))
     wetterdienst_de.start()
 
-    print("WEtter.com" + str(datetime.datetime.now()))
+    print("Wetter.com" + str(datetime.datetime.now()))
     wetter_com_crawler.start()
 
     print("Openweathermap" + str(datetime.datetime.now()))
@@ -26,4 +29,5 @@ def run():
 
 
 if __name__ == "__main__":
+    os.chdir("/home/webcrawling/webscraping_2018/")
     run()
